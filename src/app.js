35a464d33590affdc352de767805e6d5a9fbb8e8
Blargian <shaunstruwig.ss@gmail.com>
2021-03-08 18:30:12 +0200

    :heavy_plus_sign: added sass loader and setup scss support

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import Test from './components/Test';
+import './styles/styles.scss';
 
 ReactDOM.render(<Test/>,document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,52 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public/dist'),
-  },
-  module: {
-      rules: [{
-          loader: 'babel-loader',
-          test: /\.js$/,
-          exclude: /(node_modules|bower_components)/,
-          options: {
-            presets: [
-              ['@babel/preset-env', { targets: "defaults" }]
-            ]
-          }
-      }
-    ]
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true,
-    publicPath: '/dist/'
+module.exports = () => {
+
+  return {
+    entry: './src/app.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'public/dist'),
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            options: {
+              presets: [
+                ['@babel/preset-env', { targets: "defaults" }]
+              ]
+            }
+        },
+        {
+        test: /\.s?css$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap:true
+                            },
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ],
+                  }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({filename: 'styles.css'}),
+    ],
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+      publicPath: '/dist/'
+    }
   }
 };
