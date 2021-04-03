c81a70bc8417390e22c65d32bb5fbf38dfa6c1f6
Blargian <shaunstruwig.ss@gmail.com>
2021-04-03 17:14:00 +0200

    :sparkles: completed image select functionality

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -5,33 +5,43 @@ import {addImageAction} from '../actions/addActions';
 
 const ImageGrid = ({word,addImageAction}) => {
 
-    const [images, setImages] = useState([])
+    //state for storing the image url and selection state
+    const [images, setImages] = useState([{}])
+
     //on component mount fetch the images
     useEffect(async ()=>{
         const results = await axios(
             `https://pixabay.com/api/?key=${process.env.PIXA_API_KEY}&q=${word}&per_page=9`
         );
-        setImages(results.data.hits)
-        console.log(results)
+        const gridImages = [];
+        results.data.hits.forEach((image)=>{
+            gridImages.push({selected: false, url: image.webformatURL})
+        });
+        setImages(gridImages)
     },[])
 
-    const selectedImage = (imageURL) => {
-        console.log(imageURL);
+    //called when an image is clicked
+    const selectedImageHandler = (selectedIndex,imageURL) => {
+        const updatedImages = [...images];
+
+        //set all but the selected image to false
+        updatedImages.forEach((image, index)=>{
+            index === selectedIndex ? image.selected = true : image.selected=false;
+        });
+        setImages(updatedImages);
         addImageAction(imageURL);
     }
 
     return (
         <div className="image-grid">
-            {images.map((image)=>{
-                console.log(image.id)
-                console.log(image.webformatURL)
+            {images.map((image,index)=>{
                 return <div 
-                    className="image-grid__image" 
-                    key={image.id} 
-                    onClick={()=>selectedImage(image.webformatURL)}
+                    className={`image-grid__image--selected-${image.selected}`} 
+                    key={index} 
+                    onClick={()=>selectedImageHandler(index,image.url)}
                     style={{
                         content: '',
-                        backgroundImage: `url(${image.webformatURL})`,
+                        backgroundImage: `url(${image.url})`,
                         backgroundSize: 'cover',
                         backgroundPosition: 'center',
                         height: '15rem'
