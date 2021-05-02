fa0b50242f32a20674685c42a9c65956dfc28732
Blargian <shaunstruwig.ss@gmail.com>
2021-05-02 11:08:19 +0200

    :sparkles: frontend data gets passed to the python script via node child_process and stdin. Working

--- server/anki_service.py
+++ server/anki_service.py
@@ -1,4 +1,14 @@
-import sys
-# Takes first name and last name via command 
-# line arguments and then display them
-print("Ran file")
+import sys, json
+
+def read_in():
+    input = sys.stdin.readlines()
+    return json.loads(input[0])
+
+def main():
+
+    data = read_in()
+    print(data['pronounciationURL'])
+
+if __name__ == '__main__':
+    main()
+
--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 const helpers = require('../models/dbHelpers');
 
 app.use(express.static(publicPath));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
@@ -35,16 +37,17 @@ app.post('/add',interfaceAnki);
 //Function which passes the JSON to anki itself through python
 function interfaceAnki(req,res) {
 
-  console.log('Reached interfaceAnki');
+  //console.log(req.body);
   var spawn = require("child_process").spawn;
 
-  var process = spawn('python',["-u",path.join(__dirname, "anki_service.py"),
-                                req.query.firstname,
-                                req.query.lastname]);
+  var py = spawn('python',["-u",path.join(__dirname, "anki_service.py")]);
 
-  process.stdout.on('data', function(data) {
-    res.send(data.toString());
+  py.stdout.on('data', function(data) {
+    console.log(data.toString());
   });
+
+  py.stdin.write(JSON.stringify(req.body));
+  py.stdin.end();
 
 }
 
