cf00a28ab6dbfa422572ed1378ca0cce3b90712c
unknown <shaunstruwig.ss@gmail.com>
2022-10-15 20:03:13 +0200

    construction: basic adding to anki complete

--- python_app/main.py
+++ python_app/main.py
@@ -5,9 +5,23 @@ import json
 import aiohttp
 import asyncio
 import aiofiles
-import pymongo;
+import pymongo
+# for anki #
+import sys, json
+from anki.storage import Collection
+import os
+import requests
+import shutil
+#---------#
+
+#Change this to be your media directory
+anki_home = r'C:/AnkiTmp/User 1/'
+mediaDirectory=anki_home + 'collection.media'
+anki_collection_path = os.path.join(anki_home, "collection.anki2")
 
 def main(args):
+
+    #Script
     parser = argparse.ArgumentParser(description='Adds a list of words to Anki')
     parser.add_argument('-i',dest='input',help='the name of .xlsx file to run the script on. The file should be in the same directory. ')
     args = parser.parse_args()
@@ -16,6 +30,8 @@ def main(args):
     clearWordData(db)
     storeWordData(db,getWordData(db,words))
     downloadPronounciation(words,db)
+    for word in words:
+        addToAnki(db,word)
     
 # reads an excel file and retrieves the words in the file 
 def readFile(fileName):
@@ -87,7 +103,12 @@ def findWord(db,word):
             'imperfective':'',
             'perfective':'',
         },
-        'error_multipleAspectPartners': False
+        'error_multipleAspectPartners': False,
+        'pronounciationFound': False,
+        'pronounciationURL':'',
+        'imageURL':'',
+        'exampleSentence':''
+        'extraInfo'
     }
 
     inWordCollection = db["words"]
@@ -147,6 +168,61 @@ def getOppositeAspect(aspect):
     else:
         return "imperfective"
 
+#add to anki 
+def addToAnki(db,word):
+
+    try:
+        col = Collection(anki_collection_path,log=True)
+    except Exception as e:
+        print(e)
+        exit()
+
+    try:
+        modelPictureWord = col.models.by_name('2. Picture Words')
+        col.models.save(modelPictureWord)
+        deck = col.decks.by_name("Default")
+        note = col.newNote(deck['id'])
+
+        #pull the data from the temporary table in db
+        tempCollection = db["temp"]
+        data = tempCollection.find_one({'importedWord':'{}'.format(word)})
+        print(data)
+
+        #add the data to each field of the note 
+        note.fields[0] = data["accented"] #Word
+        note.fields[1] = "<img src=" + data["importedWord"]+".jpg" +"/>"  #Picture
+        note.fields[2] = "" #ExtraInfo 
+        note.fields[3] = "[sound:"+data["importedWord"]+".mp3"+"]"  #Pronounciation 
+        note.fields[4] = data["exampleSentence"] #ExampleSentence
+
+    except Exception as e:
+        print("An error occured trying to add data to note")
+        print(e)
+    
+    try:
+        copy_file('audio',data["importedWord"])
+        #download_file('image',data["imageURL"],data["accented"])
+        col.add_note(note,deck["id"])
+        col.save()
+
+    except Exception as e: 
+        print("An error occured trying to add a note")
+        print(e)
+        exit()
+
+#type 'audio' | 'image'
+def copy_file(type,word):
+    try:
+        if(type=='audio'):
+            localFile = './media/{}.mp3'.format(word)
+        elif(type=='image'):
+            localFile='./media/{}.jpg'.format(word)
+        copyTo = shutil.copy(localFile, mediaDirectory)
+        os.remove(localFile)
+    except Exception as e:
+        print("An error occured copying {}").format(type)
+        print(e)    
+        exit()
 
 if __name__ == '__main__':
     main(sys.argv)
